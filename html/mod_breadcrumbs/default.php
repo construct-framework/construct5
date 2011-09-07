819403c84ac7e970a7f9fce6ccd4bc19df768ba9
Matt Thomas <matt@betweenbrain.com>
2011-09-06 22:54:39 -0400

    HTML5 added HTML5 resets from https://github.com/murtaugh/HTML5-Reset. Tweaked a few core styles. Fixed breadcrumbs and made it's override  universal for all versions.

--- html/mod_breadcrumbs/default.php
+++ html/mod_breadcrumbs/default.php
@@ -6,8 +6,8 @@
 * @license		GNU/GPL v2 or later http://www.gnu.org/licenses/gpl-2.0.html
 */
 
-if (substr(JVERSION, 0, 3) >= '1.6') {
-// Joomla! 1.6+
+// Joomla! 1.5, 1.6+
+
 ?>
 	<nav class="breadcrumbs<?php echo $moduleclass_sfx; ?>">
 		<?php if ($params->get('showHere', 1))
@@ -21,7 +21,7 @@ if (substr(JVERSION, 0, 3) >= '1.6') {
 				// If not the last item in the breadcrumbs add the separator
 				if ($i < $count -1) {
 					if (!empty($list[$i]->link)) {
-						echo '<a href="'.$list[$i]->link.'" class="pathway">'.$list[$i]->name.'</a>';
+						echo '<li><a href="'.$list[$i]->link.'" class="pathway">'.$list[$i]->name.'</a></li>';
 					} else {
 						echo '<li>';
 						echo $list[$i]->name;
@@ -41,31 +41,3 @@ if (substr(JVERSION, 0, 3) >= '1.6') {
 			endfor; ?>
 		</ol>
 	</nav>
-	
-<?php 	
-}
-else {
-// Joomla 1.5
-?>
-
-	<nav class="breadcrumbs<?php echo $params->get('moduleclass_sfx') ?>">
-
-		<h5 class="showHere">You are here:</h5>	
-		<ol>
-			<?php for ($i = 0; $i < $count; $i ++) :	
-
-				// If not the last item in the breadcrumbs add the separator
-				if ($i < $count -1) {
-					if(!empty($list[$i]->link)) {
-						echo '<span><a href="'.$list[$i]->link.'" class="pathway">'.$list[$i]->name.'</a>';
-					} else {
-						echo '<span>'.$list[$i]->name;
-					}
-					echo ' '.$separator.' </span>';
-				}  else if ($params->get('showLast', 1)) { // when $i == $count -1 and 'showLast' is true
-					echo '<span>'.$list[$i]->name.'</span>';
-				}	
-			endfor; ?>	
-		</ol>
-	</nav>
-<?php }
