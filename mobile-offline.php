28c8386099e5cb30608676d21c0e0673391af1dc
Matt Thomas <matt@betweenbrain.com>
2011-09-16 11:00:49 -0400

    Updating to jQuery Mobile Beta 3.

--- mobile-offline.php
+++ mobile-offline.php
@@ -23,9 +23,9 @@ else {
 	<head>
 		<meta http-equiv="Content-Type" content="<?php echo $contenttype; ?>; charset=utf-8" />
 		<link rel="stylesheet" href="<?php echo $baseUrl.'templates/'.$this->template; ?>/css/mobile.css" type="text/css" media="screen" />
-		<link rel="stylesheet" href="http://code.jquery.com/mobile/1.0b2/jquery.mobile-1.0b2.min.css" />
+		<link rel="stylesheet" href="http://code.jquery.com/mobile/1.0b3/jquery.mobile-1.0b3.min.css" />
 		<script src="http://code.jquery.com/jquery-1.6.2.min.js"></script>
-		<script src="http://code.jquery.com/mobile/1.0b2/jquery.mobile-1.0b2.min.js"></script>
+		<script src="http://code.jquery.com/mobile/1.0b3/jquery.mobile-1.0b3.min.js"></script>
 		<script>
 			$(document).ready(function() {
 				$('body').removeClass("noscript");
