63ce4e8b8d1ccf80bb1747cd84d88fdf08df204c
Matt Thomas <matt@betweenbrain.com>
2011-09-23 23:16:28 -0400

    Adding layout override diagnostic value.

--- layouts/offline.php
+++ layouts/offline.php
@@ -68,6 +68,7 @@
 				
 				<?php if ($showDiagnostics) : ?>
 				<ul id="diagnostics">
+					<li>layout override</li>				
 					<li>column layout <?php echo $columnLayout; ?></li>
 					<li>component <?php echo $currentComponent; ?></li>					
 					<?php if($view)			echo '<li>'.$view.' view</li>'; ?>						
