17db8ccb6ea9f4598020e391c2e252251ea81c91
Matt Thomas <matt@betweenbrain.com>
2011-11-08 15:53:47 -0500

    Added Grid System parameter

--- elements/logic.php
+++ elements/logic.php
@@ -37,7 +37,6 @@ $view     				= JRequest::getCmd('view');
 $customStyleSheet 		= $this->params->get('customStyleSheet');
 $detectTablets			= $this->params->get('detectTablets');
 $enableSwitcher 		= $this->params->get('enableSwitcher');
-$responsive				= $this->params->get('responsive');
 $fluidMedia				= $this->params->get('fluidMedia');
 $fullWidth				= $this->params->get('fullWidth');
 $googleWebFont 			= $this->params->get('googleWebFont');
@@ -49,6 +48,7 @@ $googleWebFontTargets2	= $this->params->get('googleWebFontTargets2');
 $googleWebFont3			= $this->params->get('googleWebFont3');
 $googleWebFontSize3		= $this->params->get('googleWebFontSize3');
 $googleWebFontTargets3	= $this->params->get('googleWebFontTargets3');
+$gridSystem				= $this->params->get('gridSystem');
 $IECSS3					= $this->params->get('IECSS3');
 $IECSS3Targets			= $this->params->get('IECSS3Targets');
 $IE6TransFix			= $this->params->get('IE6TransFix');
@@ -431,10 +431,8 @@ $doc->addFavicon($template.'/apple-touch-icon.png','image/png','apple-touch-icon
 // Style sheets
 $doc->addStyleSheet($template.'/css/screen.css','text/css','screen');
 $doc->addStyleSheet($template.'/css/print.css','text/css','print');
-if ($responsive) {
-		$doc->addStyleSheet($template.'/css/responsive.css','text/css','screen');
-	} else {
-		$doc->addStyleSheet($template.'/css/grid.css','text/css','screen');
+if ($gridSystem !='-1') {
+	$doc->addStyleSheet($template.'/css/grid/'.$gridSystem,'text/css','screen');
 }
 if ($customStyleSheet !='-1') {
 	$doc->addStyleSheet($template.'/css/'.$customStyleSheet,'text/css','screen');
