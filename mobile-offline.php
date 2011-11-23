42cd81e64c0042ed4f5100f00985e45a07c4ec4a
Matt Thomas <matt@betweenbrain.com>
2011-11-22 22:27:15 -0500

    Updating jQuery Mobile to version 1.0 Updating Google Web Fonts

--- mobile-offline.php
+++ mobile-offline.php
@@ -24,9 +24,9 @@ else {
 	<head>
 		<meta http-equiv="content-type" content="text/html; charset=utf-8" />
 		<link rel="stylesheet" href="<?php echo $baseUrl.'templates/'.$this->template; ?>/css/mobile.css" type="text/css" media="screen" />
-		<link rel="stylesheet" href="http://code.jquery.com/mobile/1.0rc2/jquery.mobile-1.0rc2.min.css" />
+		<link rel="stylesheet" href="http://code.jquery.com/mobile/1.0/jquery.mobile-1.0.min.css" />
 		<script src="http://code.jquery.com/jquery-1.6.4.min.js"></script>
-		<script src="http://code.jquery.com/mobile/1.0rc2/jquery.mobile-1.0rc2.min.js"></script>
+		<script src="http://code.jquery.com/mobile/1.0/jquery.mobile-1.0.min.js"></script>
 		<script>
 			(function($) { //enable using $ along side of other libraries
 				$(document).ready(function() {
