39a65b13ffef3606895b6eec824a5133d918410e
Matt Thomas <matt@betweenbrain.com>
2012-01-05 22:56:06 -0500

    Implementing htmlspecialchars for theoretical exploit of $app->getCfg('sitename')

--- offline.php
+++ offline.php
@@ -125,7 +125,7 @@ else {
 				</ul>
 				<?php endif; ?>	
 				
-				<h1 id="logo"><a href="<?php echo $this->baseurl ?>/" title="<?php echo $app->getCfg('sitename');?>"><?php echo $app->getCfg('sitename');?></a></h1>
+				<h1 id="logo"><a href="<?php echo $this->baseurl ?>/" title="<?php echo htmlspecialchars($app->getCfg('sitename'));?>"><?php echo htmlspecialchars($app->getCfg('sitename'));?></a></h1>
 				
 				<?php if ($this->countModules('header')) : ?>
 					<jdoc:include type="modules" name="header" style="jexhtml" />	
