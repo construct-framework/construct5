fc8de225fdf37fc6a67da1c6d404ffffabf75dab
Matt Thomas <matt@betweenbrain.com>
2012-01-12 15:59:30 -0500

    Revising article header and hgroup (both conditionally loaded), adding conditional logic of displaying COM_CONTENT_ARTICLE_INFO.

--- error.php
+++ error.php
@@ -103,7 +103,7 @@ if(JFile::exists($template.'/layouts/error.php')) {
 else {
 ?>
 
-<!doctype html>
+<!DOCTYPE html>
 <!-- paulirish.com/2008/conditional-stylesheets-vs-css-hacks-answer-neither/ -->
 <!--[if lt IE 7]> <html class="no-js ie6 oldie" lang="<?php echo substr($this->language, 0, 2); ?>"> <![endif]-->
 <!--[if IE 7]>    <html class="no-js ie7 oldie" lang="<?php echo substr($this->language, 0, 2); ?>"> <![endif]-->
