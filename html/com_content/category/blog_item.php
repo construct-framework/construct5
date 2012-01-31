eab06e64cd90cdec637c374d5f1cad5f9c809729
Matt Thomas <matt@betweenbrain.com>
2012-01-31 12:06:34 -0500

    Correcting multiple display of page title on category blog layouts

--- html/com_content/category/blog_item.php
+++ html/com_content/category/blog_item.php
@@ -19,7 +19,7 @@ if (substr(JVERSION, 0, 3) >= '1.6') {
 	$images 	= json_decode($this->item->images);
 	$canEdit	= $this->item->params->get('access-edit');
 	$details    = $params->get('show_parent_category') + $params->get('show_category') + $params->get('show_create_date') + $params->get('show_modify_date') + $params->get('show_publish_date') + ($params->get('show_author') && !empty($this->item->author )) + $params->get('show_hits');
-	$header     = $details + $this->params->get('show_page_heading') + $params->get('show_title') + $params->get('show_parent_category') + $params->get('show_category');
+	$header     = $details + $params->get('show_title') + $params->get('show_parent_category') + $params->get('show_category');
 	?>
 
 		<?php if ($this->item->state == 0) : ?>
@@ -33,12 +33,6 @@ if (substr(JVERSION, 0, 3) >= '1.6') {
 	    <?php if ($header > 1) : ?>
 	    <hgroup>
 	    <?php endif; ?>
-
-		<?php if ($this->params->get('show_page_heading', 1)) : ?>
-	    <h1>
-		    <?php echo htmlspecialchars($this->params->get('page_heading')); ?>
-	    </h1>
-		<?php endif; ?>
 
 		<?php if ($params->get('show_title')) : ?>
 	    <h2>
