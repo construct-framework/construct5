b0d7584f9d45d8d8c6f1de2c8ac75fccb2ba9de9
Matt Thomas <matt@betweenbrain.com>
2012-03-02 22:39:51 -0500

    Release pre-flight. Removed final traces of .footer-text style.

--- elements/onward/upgradecheck.php
+++ elements/onward/upgradecheck.php
@@ -22,7 +22,7 @@ class JFormFieldUpgradecheck extends JFormField {
 		if(!function_exists("curl_init")) return 'cURL is not supported by your server. Please contact your hosting provider to enable this capability.';
 		//If cURL is supported, check the current version available.
 		else
-				$version = 10;
+				$version = 11;
 				$target = 'http://construct-framework.com/upgradecheck/construct5-pro';
 				$curl = curl_init();
 				curl_setopt($curl, CURLOPT_URL, $target);
