fe5afc4c561bd7c71f3d9ae012f748f2a66e704d
betweenbrain <matt@betweenbrain.com>
2012-07-13 13:09:07 -0400

    Implementing first and last classes.

--- html/mod_menu/default.php
+++ html/mod_menu/default.php
@@ -2,8 +2,8 @@
 /**
  * @version        $Id: default.php 20196 2011-01-09 02:40:25Z ian $
  * @package        Joomla.Site
- * @subpackage    mod_menu
- * @copyright    Copyright (C) 2005 - 2011 Open Source Matters, Inc. All rights reserved.
+ * @subpackage     mod_menu
+ * @copyright      Copyright (C) 2005 - 2011 Open Source Matters, Inc. All rights reserved.
  * @license        GNU General Public License version 2 or later; see LICENSE.txt
  */
 
@@ -11,71 +11,82 @@
 $templateParams = JFactory::getApplication()->getTemplate(true)->params;
 
 // Note. It is important to remove spaces between elements.
+$first = 0;
 ?>
 
 <ul data-role="listview" data-inset="true" data-theme="<?php echo $templateParams->get('mNavDataTheme'); ?>" class="menu<?php echo $class_sfx;?> clearfix"<?php
-        $tag = '';
-        if ($params->get('tag_id') != NULL) {
-            $tag = $params->get('tag_id') . '';
-            echo ' id="' . $tag . '"';
-        }
-        ?>>
-    <?php
-    foreach ($list as $i => &$item) :
-        $class = 'item-' . $item->id;
-        if ($item->id == $active_id) {
-            $class .= ' current';
-        }
+		$tag = '';
+		if ($params->get('tag_id') != NULL) {
+			$tag = $params->get('tag_id') . '';
+			echo ' id="' . $tag . '"';
+		}
+		?>>
+	<?php
+	foreach ($list as $i => &$item) :
+		$class = 'item-' . $item->id;
+		if ($item->id == $active_id) {
+			$class .= ' current';
+		}
 
-        if (in_array($item->id, $path)) {
-            $class .= ' active';
-        }
-        elseif ($item->type == 'alias') {
-            $aliasToId = $item->params->get('aliasoptions');
-            if (count($path) > 0 && $aliasToId == $path[count($path) - 1]) {
-                $class .= ' active';
-            }
-            elseif (in_array($aliasToId, $path)) {
-                $class .= ' alias-parent-active';
-            }
-        }
+		if (in_array($item->id, $path)) {
+			$class .= ' active';
+		} elseif ($item->type == 'alias') {
+			$aliasToId = $item->params->get('aliasoptions');
+			if (count($path) > 0 && $aliasToId == $path[count($path) - 1]) {
+				$class .= ' active';
+			} elseif (in_array($aliasToId, $path)) {
+				$class .= ' alias-parent-active';
+			}
+		}
 
-        if ($item->parent) {
-            $class .= ' parent';
-        }
+		if ($item->parent) {
+			$class .= ' parent';
+		}
 
-        if (!empty($class)) {
-            $class = ' class="' . trim($class) . '"';
-        }
+		if ($item->deeper) {
+			$first = -1;
+		}
 
-        echo '<li' . $class . '>';
+		if ($first == 0) {
+			$class .= ' first';
+		}
 
-        // Render the menu item.
-        switch ($item->type) :
-            case 'separator':
-            case 'url':
-            case 'component':
-                require JModuleHelper::getLayoutPath('mod_menu', 'default_' . $item->type);
-                break;
+		if ($i == (count($list) + 1) || $item->shallower) {
+			$class .= ' last';
+		}
 
-            default:
-                require JModuleHelper::getLayoutPath('mod_menu', 'default_url');
-                break;
-        endswitch;
+		if (!empty($class)) {
+			$class = ' class="' . trim($class) . '"';
+		}
 
-        // The next item is deeper.
-        if ($item->deeper) {
-            echo '<ul>';
-        }
-        // The next item is shallower.
-        elseif ($item->shallower) {
-            echo '</li>';
-            echo str_repeat('</ul></li>', $item->level_diff);
-        }
-        // The next item is on the same level.
-        else {
-            echo '</li>';
-        }
-    endforeach;
-    ?></ul>
+		echo '<li' . $class . '>';
+
+		// Render the menu item.
+		switch ($item->type) :
+			case 'separator':
+			case 'url':
+			case 'component':
+				require JModuleHelper::getLayoutPath('mod_menu', 'default_' . $item->type);
+				break;
+
+			default:
+				require JModuleHelper::getLayoutPath('mod_menu', 'default_url');
+				break;
+		endswitch;
+
+		// The next item is deeper.
+		if ($item->deeper) {
+			echo '<ul>';
+		} // The next item is shallower.
+		elseif ($item->shallower) {
+			echo '</li>';
+			echo str_repeat('</ul></li>', $item->level_diff);
+		}
+		// The next item is on the same level.
+		else {
+			echo '</li>';
+		}
+		$first++;
+	endforeach;
+	?></ul>
 
